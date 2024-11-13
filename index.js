a3f1474962c04a28eccfc25e3518c7064759827b
RoaringKitty0 <vvlipihina2010@mail.ru>
2024-11-13 16:23:25 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function scrollToSection(sectionId) {
+    const section = document.querySelector(sectionId);
+    if (section) {
+        section.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    }
+}
