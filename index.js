7e56dc413c7423354fc9bde01d6ba73d9a1fca5e
RoaringKitty0 <vvlipihina2010@mail.ru>
2024-11-24 14:51:01 +0500

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-function scrollToSection(sectionId) {
-    const section = document.querySelector(sectionId);
-    if (section) {
-        section.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        });
-    }
-}
